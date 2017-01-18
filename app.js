8ec772fd32627bd7bdcfe0784a38e67efe4b6a79
Jean-Louis Leysens <jloleysens@gmail.com>
2017-01-18 11:23:21 +0200

    Restructuring and refactoring

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var async = require('asyncawait/async');
 var await = require('asyncawait/await');
 var Promise = require('bluebird');
 Getopt = require('node-getopt');
+var config = require('./config.json');
 
 var basic_auth_header = function(u, p) {
     return {
@@ -20,37 +21,59 @@ var basic_auth_header = function(u, p) {
 
 getopt = new Getopt([
     ['o', 'output=ARG', 'The path to which to output files. Defaults to \'./\'.'],
+    ['atms', 'atmstart=ARG', 'The start date of the ATM jobs'],
+    ['atme', 'atmend=ARG', 'The end date of the ATM jobs'],
+    ['abms', 'abmstart=ARG', 'The start date of the ABM jobs'],
+    ['abme', 'abmend=ARG', 'The end date of the ABM jobs'],
     ['h', 'help', 'Display this help message']
 ]);
 
 opt = getopt.parse(process.argv.slice(2));
 
-function init(){
-    // Config Stuff
-    if(opt.options.help) return getopt.showHelp();
-    var backends = require('./config.json').app_backends;
 
-    for(var i = 0; i < backends.length; i++){
-        if(typeof backends[i].base_url === 'undefined') continue;
-        console.log('Fetching job data for: %s', backends[i].name);
-        fetchCount(backends[i])
-            .then(function(resp){
-                console.log(resp[0] + ' jobs found...');
-                batchifyDataRequests(resp[0], resp[1]);
-            })
-            .catch(function(err){ throw new Error(err.message); });
-    }
+var monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+var job_name = [
+    "ABM",
+    "ATM"
+];
+
+function init(){
+    if(opt.options.help) return getopt.showHelp();
+
+    console.log('Starting fetch requests...');
+    batchifyDataRequests();
 }
 
-function batchifyDataRequests(count, backend, skip){
-    if(typeof skip === 'undefined') skip = 0;
+function batchifyDataRequests(){
+    if(typeof skip === 'undefined') skip_abm = 0;
+    if(typeof skip === 'undefined') skip_atm = 0;
+    var atm_done = false;
+    for(var i = 0; i < config.max_batch; i++){
+    }
     fetchData(count, backend, skip)
         .then(function (data){ console.log("Data fetched."); })
         .catch(function(err){ console.log(err.message); });
 }
 
+function writeToCSV(file_name) {
+}
+
 var fetchData = async (function (count, backend, skip){
-    var url = backend.base_url + "/job.json?limit=500&skip=" + skip;
+    var url = backend.base_url + "/job.json?limit=" + config.max_request +"&skip=" + skip;
     var options = { headers: basic_auth_header(backend.username, backend.password), method: 'get' };
 
     var data = fetch(url, options)
@@ -64,20 +87,20 @@ var fetchData = async (function (count, backend, skip){
     return [await(data), count, backend, skip];
 });
 
-var fetchCount = async (function (backend){
-    var url = backend.base_url + "/job/count.json";
-    var options = { headers: basic_auth_header(backend.username, backend.password), method: 'get' };
-
-    var count = fetch(url, options)
-        .then( function (response){
-            if(response) return response.json();
-            else throw new Error("No response object.");
-        })
-        .then( function(json){
-            return json.count;
-        });
-    return [await (count), backend];
-});
+// var fetchCount = async (function (backend){
+//     var url = backend.base_url + "/job/count.json";
+//     var options = { headers: basic_auth_header(backend.username, backend.password), method: 'get' };
+//
+//     var count = fetch(url, options)
+//         .then( function (response){
+//             if(response) return response.json();
+//             else throw new Error("No response object.");
+//         })
+//         .then( function(json){
+//             return json.count;
+//         });
+//     return [await (count), backend];
+// });
 
 function basicHash(uname, pword){
     return new Buffer(uname + ":" + pword).toString('base64');
